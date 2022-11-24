dcbc9254c34df162c036c0519c4362eb27f394a0
Astryp212 <cryptodev713@gmail.com>
2022-11-24 07:58:32 -0600

    fix issue

--- ui/src/components/LifiWidget/index.tsx
+++ ui/src/components/LifiWidget/index.tsx
@@ -1,13 +1,22 @@
 import { LiFiWidget, WidgetConfig } from "@lifi/widget";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export const LifiWidget = () => {
+  const [count, setCount] = useState(1);
   const widgetConfig: WidgetConfig = useMemo(() => {
     return {
       integrator: "valve-ui",
       variant: "drawer",
     };
   }, []);
+
+  useEffect(() => {
+    if (count < 4) {
+      setCount(count + 1);
+    }
+  }, [count]);
+
+  if (count % 2 === 1) return null;
 
   return <LiFiWidget config={widgetConfig} />;
 };
